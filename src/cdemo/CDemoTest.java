47b43103933d49d8fc45371409e200f0ea293118
nasrinna <nasrin.ahmad@noorteck.com>
2025-01-31 21:29:02 -0500

    added method

--- src/cdemo/CDemoTest.java
+++ src/cdemo/CDemoTest.java
@@ -4,6 +4,14 @@ public class CDemoTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Team 3");
+		
+		
+		
+		int a = 20;
+		
+		int b = 25;
+		
+		System.out.println(a + b);
 	}
 	
 }
