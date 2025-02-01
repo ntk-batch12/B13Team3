f28a5164dd45affbfa78fe36ba3b2d0b1019763e
carol <carol@Carolina>
2025-01-31 21:33:48 -0500

    Add new message

--- src/cdemo/CDemoTest.java
+++ src/cdemo/CDemoTest.java
@@ -4,6 +4,13 @@ public class CDemoTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Team 3");
+		
+		
+		String lastname = "Tolosa";
+		
+		System.out.println(lastname);
+		
+		
 	}
 	
 }
