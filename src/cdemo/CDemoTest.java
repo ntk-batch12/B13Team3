798dea0bf5d59daf7a9305a4c2575c5ab965462b
vonde <vonde@DoggonGecko.lan>
2025-02-01 11:32:24 -0500

    I added Instance Variables

--- src/cdemo/CDemoTest.java
+++ src/cdemo/CDemoTest.java
@@ -1,24 +1,32 @@
 package cdemo;
 
 public class CDemoTest {
-	
+			// instance variable
+			public String firstName = "Zaid";
+			private String lastName = "David";
+			protected char gender = 'M';
+			int id = 123;
+			
+			// static variable
+			public static String country = "USA";
+			private static String state = "FL";
+			protected static boolean isCitizen = true;
+			double salary = 1111.11;
+		
+
 	public static void main(String[] args) {
 		System.out.println("Team 3");
-		
-		
 
 		String lastname = "Tolosa";
-		
+		String firstName = "";
+
 		System.out.println(lastname);
-		
-		
-           int a = 20;
-           int b = 22;
-           
-           System.out.println(a+b);
+
+		int a = 20;
+		int b = 22;
+
+		System.out.println(a + b);
 	}
 // Try a new comment //
-       
 
-	
 }
